12b06cbf6deb6bef3ac04ebddcef721db46d4420
connery0 <onmymarkburn4321burn@gmail.com>
2014-09-03 14:33:42 +0200

    cleaned up code + explenation

--- launchpad/objects/launchpad/launchpad.lua
+++ launchpad/objects/launchpad/launchpad.lua
@@ -1,23 +1,37 @@
 function init(args)
 	entity.setInteractive(true)
 	self.jump=false
+	self.hop=false
 end
 function main()
-	self.testVar = world.playerQuery( entity.position(), 100)
+
+	-- save closest player
+	self.closestPlayer = world.playerQuery( entity.position(), 100)
 	
 	if self.jump then
-	self.jump=false
-	local p = entity.toAbsolutePosition({ -1.3, 1 })
-	world.logInfo("x1= "..tostring(p[1]))
-	world.logInfo("y1= "..tostring(p[2]))
-	world.logInfo("x1= "..tostring(p[1])+40)
-	world.logInfo("y1= "..tostring(p[2])+80)
-
-	--try out 40000,40000 :P
-	local A = {0,4000} -- first = horizontal movement   second= vertical
+		self.jump=false
+		
+		self.hop=true
+		
+		-- point that has to be at the ?bottom left? of the object... used to set force region
+		local p = entity.toAbsolutePosition({ -1.3, 1 })
 	
-    entity.setForceRegion({ p[1], p[2], p[1] + 40, p[2]+80 }, { 0,100})	
-    entity.setForceRegion({ p[1], p[2], p[1] + 40, p[2]+80 }, { A[1],A[2]})
+	
+		-- Puts some stuff in the starbound log about positions (mostly to find problems) 
+		--These positions are where the force is aplied
+		world.logInfo("x1= "..tostring(p[1]))
+		world.logInfo("y1= "..tostring(p[2]))
+		world.logInfo("x1= "..tostring(p[1])+40)
+		world.logInfo("y1= "..tostring(p[2])+80)
+
+		-- Force to be used for jumping (what direction) {horizontalPower,verticalPower}
+		local JumpForce = {0,4000} 
+		
+		-- Aplies force at the given location  setForceRegion( {4 points to determine rectangle area},{force})
+			-- test to lift the player up
+		entity.setForceRegion({ p[1], p[2], p[1] + 40, p[2]+80 }, { 0,100})	
+			-- actual launch
+		entity.setForceRegion({ p[1], p[2], p[1] + 40, p[2]+80 }, { JumpForce[1],JumpForce[2]})
 	end
 	
 end
@@ -25,21 +39,30 @@ end
 
 
 function onInboundNodeChange(args)
-  onInteraction(args)
+	onInteraction(args)
 end
 
 function onInteraction(args)
 	
-self.jump=true
-world.logInfo(tostring(self.testVar))
-if #self.testVar>0 then
-self.playerPos=world.entityPosition(self.testVar[1])
-world.logInfo(tostring(self.testVar[1]))
-world.logInfo(tostring(self.playerPos[1]))
-world.logInfo(tostring(self.playerPos[2]))
+	self.jump=true
 
-end
-world.logInfo("--------------------------------------------------")
+	world.logInfo(tostring(self.closestPlayer))
+
+	-- only try to set self.playerPos if there  is a player close by.
+	if #self.closestPlayer>0 then
+
+		-- Save the current position of the player
+		self.playerPos=world.entityPosition(self.closestPlayer[1])
+	
+		-- piontles info to show in starbound.log
+		world.logInfo("closest Player [1]= "..tostring(self.closestPlayer[1]))
+		world.logInfo(tostring(self.playerPos[1]))
+		world.logInfo(tostring(self.playerPos[2]))
+
+	end
+
+	-- seperate the info of each interaction
+	world.logInfo("--------------------------------------------------")
 
 
 end
